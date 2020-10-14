901027547d0ab9812edb1c94fe5673e8b58c5486
LucasFerreira14 <ferreira.silva5641@gmail.com>
2020-10-14 20:33:12 -0300

    AC3

--- primos.py
+++ primos.py
@@ -38,5 +38,5 @@ def nao_entre_em_panico():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("port", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
